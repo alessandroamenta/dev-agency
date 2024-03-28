8ef0d644b162b5acc02ad4dc398154dfeac645c1
alessandroamenta <alessandroamenta1@gmail.com>
2024-03-28 20:34:55 +0100

    Add @vercel/analytics package and import Analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 import { Poppins } from 'next/font/google'
 
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>{children}
+      <Analytics />
       </body>
     </html>
   );
